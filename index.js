d795d4193d0bc9271161afb65ef39cfc831061f4
unknown <hibakashifkhan1@gmail.com>
2024-07-13 19:38:49 +0500

    Adding My TypeScript Practice

---
+++ index.js
@@ -0,0 +1,33 @@
+//Making School Timetable using switch case
+import inquirer from "inquirer";
+let day = await inquirer.prompt([
+    {
+        name: "days",
+        type: "list",
+        message: "Choose a Day",
+        choices: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    }
+]);
+switch (day.days) {
+    case "Monday":
+        console.log("Study Maths Today");
+        break;
+    case "Tuesday":
+        console.log("Study Science Today");
+        break;
+    case "Wednesday":
+        console.log("Study History Today");
+        break;
+    case "Thursday":
+        console.log("Study English Today");
+        break;
+    case "Friday":
+        console.log("Study Geography Today");
+        break;
+    case "Saturday":
+        console.log("Study Physics Today");
+        break;
+    default:
+        console.log("Play Games");
+        break;
+}
